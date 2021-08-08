7d08e82ed68fa89354167d4bcd5698d6e72c596c
ToVic <61870120+ToVic@users.noreply.github.com>
2021-08-08 23:08:08 +0200

    remove special case ahndling from text_preprocessing

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -5,7 +5,7 @@ def remove_special_characters(sentence) -> str:
     Takes in a string and returns the same string with special characters removed.
     """
     if type(sentence) == str:
-        special_cases_handled = re.sub(r"[\/\-\_]", " ", sentence)
-        return re.sub(r"[^A-Za-z0-9,.?!]", r"", special_cases_handled)
+        # special_cases_handled = re.sub(r"[\/\-\_]", r" ", sentence)
+        return re.sub(r"[^A-Za-z0-9,.?! ]", r"", sentence)
 
     return "Input is not a string!"
