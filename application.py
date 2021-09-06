f8792feed961eb63253602029aec0a96c69fee8a
Araz1103 <arazsharma1103@gmail.com>
2021-09-06 07:45:57 +0530

    All Cluster Labelling Changes

--- application.py
+++ application.py
@@ -207,13 +207,34 @@ def handleUSECluster3():
     if(len(data_used_for_demo)<50):
         
         best_scores = list(map(int,best_score_HAC_sparse(embeddings, data_used_for_demo, 2)[1]))
+<<<<<<< Updated upstream
         return jsonify(return_cluster_dict(best_scores,q_ids_list))
+=======
+        final_list = return_cluster_dict(best_scores,q_ids_list)
+        return_cluster_labels_NMI_nGrams_Centroid(embeddings, data_used_for_demo, q_ids_list, final_list)
+        for cluster_id in final_list:
+            q_ids = final_list[cluster_id]
+            final_list[cluster_id] = {"q_ids":q_ids, "label":" "}
+        return jsonify(final_list)
+>>>>>>> Stashed changes
     
         
     else:
         
         best_scores = list(map(int,get_best_HAC_normal(embeddings, data_used_for_demo)[1]))
+<<<<<<< Updated upstream
         return jsonify(return_cluster_dict(best_scores,q_ids_list))
+=======
+        print(q_ids_list)
+        print(best_scores)
+        final_list = return_cluster_dict(best_scores,q_ids_list)
+        print(final_list)
+        return_cluster_labels_NMI_nGrams_Centroid(embeddings, data_used_for_demo, q_ids_list, final_list)
+        for cluster_id in final_list:
+            q_ids = final_list[cluster_id]
+            final_list[cluster_id] = {"q_ids":q_ids, "label":" "}
+        return jsonify(final_list)
+>>>>>>> Stashed changes
     
         
 
