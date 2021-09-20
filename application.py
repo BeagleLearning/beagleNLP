649dad07ae8e7012c705565e070eda889bbfb59c
Araz1103 <arazsharma1103@gmail.com>
2021-09-21 01:40:31 +0530

    Function Names Merge Request Changes

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ import numpy as np
 from build_tag_cluster import buildTagCluster
 from beagleError import BeagleError
 import errors
-from use_cluster import get_data_embeddings, best_score_HAC_sparse, HAC_with_Sparsification, get_best_HAC_normal, return_cluster_dict,return_cluster_labels_NMI_nGrams_Centroid
+from use_cluster import get_data_embeddings, best_score_HAC_sparse, HAC_with_Sparsification, get_best_HAC_normal, return_cluster_dict,return_cluster_labels_nmi_ngrams_centroid
 import time
 from functools import wraps
 
@@ -210,7 +210,7 @@ def handleUSECluster3():
         
         best_scores = list(map(int,best_score_HAC_sparse(embeddings, data_used_for_demo, 2)[1]))
         final_list = return_cluster_dict(best_scores,q_ids_list)
-        labels = return_cluster_labels_NMI_nGrams_Centroid(embeddings, data_used_for_demo, q_ids_list, final_list)
+        labels = return_cluster_labels_nmi_ngrams_centroid(embeddings, data_used_for_demo, q_ids_list, final_list)
         for cluster_id in final_list:
             q_ids = final_list[cluster_id]
             cluster_label = ' , '.join(labels[cluster_id])
@@ -222,7 +222,7 @@ def handleUSECluster3():
         
         best_scores = list(map(int,get_best_HAC_normal(embeddings, data_used_for_demo)[1]))
         final_list = return_cluster_dict(best_scores,q_ids_list)
-        labels = return_cluster_labels_NMI_nGrams_Centroid(embeddings, data_used_for_demo, q_ids_list, final_list)
+        labels = return_cluster_labels_nmi_ngrams_centroid(embeddings, data_used_for_demo, q_ids_list, final_list)
         for cluster_id in final_list:
             q_ids = final_list[cluster_id]
             cluster_label = ' , '.join(labels[cluster_id])
